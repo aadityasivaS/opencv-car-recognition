fa6d9a08e3eb82eb44423c7f6a5832116a9165a1
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2020-11-08 13:32:19 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -9,10 +9,13 @@ Created on Thu Nov  5 21:26:24 2020
 import cv2
 from matplotlib import pyplot as plt
 import easyocr
+import requests
 reader = easyocr.Reader(['en'])
 
 car_cascade = cv2.CascadeClassifier('cars.xml')
-
+r = requests.get(url = "https://us-central1-smart-car-parking-system-b6c28.cloudfunctions.net/number")
+data = r.json()
+print(data)
 
 vid = cv2.VideoCapture(0) 
   
@@ -28,10 +31,17 @@ while(True):
 
     for (x, y, w, h) in cars:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
-        print(reader.readtext(frame, detail = 0));
+        text = (reader.readtext(frame, detail = 0))
+        if text != []:
+            print(text[0])
+            a = str(text[0])
+            try:
+                print(data[a])
+            except:
+                print('Invalid number')
         cars = cars + 1
 
-    plt.figure(figsize=(10,20))
+    #plt.figure(figsize=(10,20))
     plt.imshow(frame)
     cv2.imshow('frame', frame) 
     # the 'q' button is set as the 
